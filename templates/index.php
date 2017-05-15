a3b70bbfc2946e7e3b41a18f72d2f3ecd422a441
KerstinGraudins <kgraud01@seattlecentral.edu>
2017-05-15 14:14:06 -0700

    Creating a Custom Posts Feed with Featured Images in WordPress

--- templates/index.php
+++ templates/index.php
@@ -9,53 +9,21 @@ version 1.0
 
 <?php get_header(); ?>
 
-
    <!-- begin wp loop -->
+<div id="content"
     <?php if ( have_posts()) :while(have_posts()) : the_post(); //starts loop ?>
+     <article class="post-excerpt">
     <h2><a href="<?php the_permalink();//link to page?>">
         <?php the_title(); //get title ?></a></h2>
-    <?php the_content(''); //gets content ?>
+<small>Posted on <?php the_time('F j, Y'); //get the time ?> by <?php the_author(); //gets authors name ?> in <?php the_category(', ');//gets category ?></small>
+<a href="<?php the_permalink(); //links to page or posting ?>">
+<?php the_post_thumbnail( 'thumbnail' ); ?>
+  <?php the_exerpt(); //gets the postings exerpt ?>
     <!--page content goes here  -->
+</article>
         <?php endwhile; endif;  //ends loop ?>
     <!-- end wp loop -->
-</div><!--end wrapper div  -->
-
-<div id="banner">
-<!-- blueberry -->
-
-    <div class="blueberry">
-      <ul class="slides">
-        <li><img src="images/kade.jpg" alt="Kerstin Graudins print" /></li>
-        <li><img src="images/queer.jpg" alt="silent protest" /></li>
-        <li><img src="images/goldface.jpg" alt="Mark Mitchell" /></li>
-        <li><img src="images/camik.jpg" alt="Imminent Mode 2015" /></li>
-      </ul>
-    </div>
-
-<!-- blueberry -->
-</div>
-
-
-<div class="article column1">
-<p><h2>Ade' Connere is a multi-talented artist. She is an actress, singer and musician.</h2></p>
-</div>
-<div class="article column2">
-<h2>LINKS</h2>
-<a href="http://www.thestranger.com/blogs/slog/2015/03/13/21901695/the-little-magic-that-remains-in-our-changing-capitol-hill"><h3>The Stranger</h3></a>
-<a href="https://www.bellevuecollege.edu/news/2016/05/17/drag-bingo-coming-to-bellevue-college/"><h3>Bingo Host</h3></a>
-<a href="http://chopsuey.com/calendar/cocktails-couture-the-witching-hour-in-the-den/"><h3>Chop Suey</h3></a>
-<a href="https://www.strangertickets.com/events/33096464/whatever-happened-to-bianca-del-rio"><h3>Theater</h3></a>
-<a href="http://www.thestranger.com/blogs/slog/2015/03/13/21901695/the-little-magic-that-remains-in-our-changing-capitol-hill"><h3>The Stranger</h3></a>
-</div>
-<div class="article column3">
-<form action="formhandler.php"
-method="post">
-<fieldset>
-<label>SEND ME A MESSAGE</label>
-<textarea name= "comments" rows="8"></textarea>
-<input type="submit" value="submit">
-</fieldset>
-</form>
+<small>index.php</small>
 </div>
 
 <?php get_footer(); ?>
